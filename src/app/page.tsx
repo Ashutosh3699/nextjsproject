685174ee7f8c2214b39579017784d5511e2264ac
Ashutosh3699 <bishoiashutosh@gmail.com>
2024-10-07 00:07:36 +0530

    Navbar created commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen  bg-black/[0.96]  antialiased  bg-grid-white/[0.02] ">
+        <h2  className=" text-2xl text-gray-50 font-bold text-center">
+          Ashutosh Here,
+      </h2>
+    </main>
+  );
+}
